e13a759eba117c026a283a8095139fe0b96b1d05
jplealj <37347503+jplealj@users.noreply.github.com>
2021-06-28 15:00:45 -0500

    Update

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -6,8 +6,8 @@ export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
   seleniumAddress: 'http://localhost:4444/wd/hub'
+  onPrepare: () => {
+    reporter();
+  }
 };
 
-onPrepare: () => {
-  reporter();
-}
