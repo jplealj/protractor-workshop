799d42a217bfb47bdb9324c33cc69b4d41ca4339
Juan Leal Jaramillo <juan.lealj@perficient.com>
2021-06-29 09:50:34 -0500

    module 5

---
+++ protractor/headless.config.ts
@@ -0,0 +1,19 @@
+import { Config } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+
+export const config: Config = {
+  framework: 'jasmine',
+  specs: [ '../test/google.spec.js' ],
+  SELENIUM_PROMISE_MANAGER: false,
+  onPrepare: () => {
+    reporter();
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  }
+};
+
