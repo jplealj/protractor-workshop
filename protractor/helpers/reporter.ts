eb0dfcbe3eb58bf7be2126acb05e25e012d19662
jplealj <37347503+jplealj@users.noreply.github.com>
2021-07-07 16:23:55 -0500

    Module 16: provisional (#16)
    * Module 16: provisional
    
    * update fix
    
    * update del update

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,6 +1,15 @@
 import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
 
+const { AwesomeReport } = require('jasmine-awesome-report');
+
+const config = {
+  fullPath: 'reports',
+  fileName: 'awesome',
+  merge: true,
+};
+
 export const reporter = () => {
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
       displayStacktrace: StacktraceOption.PRETTY,
