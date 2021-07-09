5615ec31fcd8a929149de7451aac295543219af0
jplealj <37347503+jplealj@users.noreply.github.com>
2021-07-09 15:01:44 -0500

    PR Module 18 (#18)
    * module 18
    
    * update

--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,16 +1,33 @@
 import {
   $, browser, ElementFinder, ExpectedConditions,
+  ElementArrayFinder, $$,
 } from 'protractor';
 
 export class ProductListPage {
   private addToCart: ElementFinder;
 
+  private products: ElementArrayFinder;
+
   constructor() {
-    this.addToCart = $('#center_column a.button.ajax_add_to_cart_button.btn.btn-default');
+    this.addToCart = $('#add_to_cart');
+    this.products = $$('.product_list');
   }
 
   public async addToCartP(): Promise<void> {
     await browser.wait(ExpectedConditions.elementToBeClickable(this.addToCart), 3000);
     await this.addToCart.click();
+  }
+
+  private findByProduct(productName: string): ElementFinder {
+    return this.products.filter((product:ElementFinder) => product
+      .$('.product-name').getText()
+      .then((name: string) => name.includes(productName)))
+      .first();
+  }
+
+  public async selectProduct(productName: string): Promise<void> {
+    const product = this.findByProduct(productName);
+    await browser.wait(ExpectedConditions.elementToBeClickable(product.$('img')), 5000);
+    await product.$('img').click();
   }
 }
